c068507135673273ec5ad7b913fa0880e4a54619
Thomagotchi <thomassano87@gmail.com>
2024-07-22 18:24:19 +0200

    Instalation JWT

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const app = express();
+const bookRoute = require('./routes/bookRouter')
+const userRoute = require('./routes/userRouter')
 
 mongoose.connect('mongodb+srv://admin:admin@mon-vieux-grimoire.gzech7n.mongodb.net/?retryWrites=true&w=majority&appName=mon-vieux-grimoire',
     { useNewUrlParser: true,
       useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+app.use(express.json())
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
@@ -22,8 +26,9 @@ app.use((req, res, next) => {
     next()
 })
 
-app.use('/api/books', (req, res) => {
-    req.json({ message: ' Rq recu ' })
-})
+
+
+app.use('/api/auth', userRoute)
+app.use('/api/books', bookRoute)
 
 module.exports = app;
