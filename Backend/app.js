5c35a57fd28ca27137b640b2b6d1ee8429c95644
Thomagotchi <thomassano87@gmail.com>
2024-07-22 11:59:56 +0200

    Connexion MongoDB

--- Backend/app.js
+++ Backend/app.js
@@ -1,24 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://admin:admin@mon-vieux-grimoire.gzech7n.mongodb.net/?retryWrites=true&w=majority&appName=mon-vieux-grimoire',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use((req, res, next) => {
-    console.log('rq recu')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    )
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    )
     next()
 })
 
-app.use((req, res, next) => {
-    res.status(201)
-    next()
-})
-
-app.use((req, res, next) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
-   next()
-});
-
-app.use((req, res) => {
-    console.log('Rq bien envoyé')
+app.use('/api/books', (req, res) => {
+    req.json({ message: ' Rq recu ' })
 })
 
 module.exports = app;
